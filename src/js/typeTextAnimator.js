e4261e2b2ad9b7209b4bf00021c814c57edaee39
Ruichimi <thekeykeyloy@gmail.com>
2025-08-31 18:55:46 +0200

    Synchronize animations in different browsers

--- src/js/typeTextAnimator.js
+++ src/js/typeTextAnimator.js
@@ -1,7 +1,7 @@
 // Rewritten version of the code from https://codepen.io/hi-im-si/pen/ALgzqo
 
 class TypeWriter {
-    constructor(element, texts, speed = 15, delay = 2000, showCursor = true) {
+    constructor(element, texts, speed = 50, delay = 2000, showCursor = true) {
         this.element = element;                        //DOM element with text
         this.currentIndex = 0;                         //Index of the current text being typed
         this.currentText = '';                         //Current text being typed
@@ -10,7 +10,8 @@ class TypeWriter {
         this.speed = parseInt(speed, 10) || 50;   //Speed of typing and removing in ms
         this.delay = parseInt(delay, 10);         //How long to wait between removing typing each text
 
-        this.timeoutId = null;
+        this.rafId = null;                             //requestAnimationFrame id
+        this.lastTime = 0;                             //Last timestamp for step()
 
         this.enableTextCursorIfNeeded(showCursor);
 
@@ -20,35 +21,55 @@ class TypeWriter {
     typeNext() {
         const fullText = this.texts[this.currentIndex % this.texts.length];
         this.addText(fullText, () => {
-            this.timeoutId = setTimeout(() => {
+            setTimeout(() => {
                 this.removeText(() => {
                     this.currentIndex++;
-                    this.timeoutId = setTimeout(() => this.typeNext(), 500);
+                    setTimeout(() => this.typeNext(), 500);
                 });
             }, this.delay);
         });
     }
 
     addText(fullText, callback) {
-        this.clearTimeoutIfExists();
-        if (this.currentText.length < fullText.length) {
-            this.currentText = fullText.substring(0, this.currentText.length + 1);
-            this.updateElement();
-            this.timeoutId = setTimeout(() => this.addText(fullText, callback), this.speed);
-        } else if (callback) {
-            callback();
-        }
+        this.cancelAnimationIfExists();
+        this.lastTime = performance.now();
+
+        const step = (now) => {
+            if (now - this.lastTime >= this.speed) {
+                this.currentText = fullText.substring(0, this.currentText.length + 1);
+                this.updateElement();
+                this.lastTime = now;
+            }
+
+            if (this.currentText.length < fullText.length) {
+                this.rafId = requestAnimationFrame(step);
+            } else if (callback) {
+                callback();
+            }
+        };
+
+        this.rafId = requestAnimationFrame(step);
     }
 
     removeText(callback) {
-        this.clearTimeoutIfExists();
-        if (this.currentText.length > 0) {
-            this.currentText = this.currentText.substring(0, this.currentText.length - 1);
-            this.updateElement();
-            this.timeoutId = setTimeout(() => this.removeText(callback), this.speed / 3);
-        } else if (callback) {
-            callback();
-        }
+        this.cancelAnimationIfExists();
+        this.lastTime = performance.now();
+
+        const step = (now) => {
+            if (now - this.lastTime >= this.speed / 3) {
+                this.currentText = this.currentText.substring(0, this.currentText.length - 1);
+                this.updateElement();
+                this.lastTime = now;
+            }
+
+            if (this.currentText.length > 0) {
+                this.rafId = requestAnimationFrame(step);
+            } else if (callback) {
+                callback();
+            }
+        };
+
+        this.rafId = requestAnimationFrame(step);
     }
 
     updateElement() {
@@ -56,20 +77,18 @@ class TypeWriter {
     }
 
     enableTextCursorIfNeeded(showCursor) {
-        console.log('meow', showCursor);
-        console.log(this.element);
         if (showCursor) this.element.style.border = '0.08em solid #fff';
     }
 
-    clearTimeoutIfExists() {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-            this.timeoutId = null;
+    cancelAnimationIfExists() {
+        if (this.rafId) {
+            cancelAnimationFrame(this.rafId);
+            this.rafId = null;
         }
     }
 
     stop() {
-        this.clearTimeoutIfExists();
+        this.cancelAnimationIfExists();
         this.currentText = '';
         this.updateElement();
     }
