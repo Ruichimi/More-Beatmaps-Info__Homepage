ca6e4cdfbe98716e4fa46e8586ca0293c8371326
Ruichimi <thekeykeyloy@gmail.com>
2025-12-04 20:22:50 +0100

    Content glow effects, navbar improvements, more fonts

--- src/js/navbarController.js
+++ src/js/navbarController.js
@@ -10,30 +10,32 @@ function toggleMenu(menu) {
     mobileMenuDisplayed = !mobileMenuDisplayed;
 }
 
-function navbarMobileStyleUpdate(navbarLogo, navbarBar) {
+function navbarMobileStyleUpdate(navbarLogo, navbarBar, borderHider) {
     navbarBar.classList.toggle('border-l-0');
-    navbarBar.classList.toggle('border-l-2');
+    navbarBar.classList.toggle('border-l-3');
     navbarBar.classList.toggle('rounded-bl-none');
-    navbarBar.classList.toggle('right-[0.125rem]');
+
     navbarLogo.classList.toggle('rounded-br-none');
     navbarLogo.classList.toggle('border-r-0');
     navbarLogo.classList.toggle('z-50');
-    navbarLogo.classList.toggle('left-[0.125rem]');
+    navbarLogo.classList.toggle('left-[3px]');
+
+    borderHider.classList.toggle('hidden');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const navbarLogo = document.getElementById('navbar-logo');
     const navbarBar = document.getElementById('navbar-bar');
+    const borderHider = document.getElementById('border-hider');
     const menu = document.getElementById('mobile-menu');
     const btn = document.getElementById('menu-btn');
-
 
     function updateWidth() {
         console.log(window.innerWidth);
         const width = window.innerWidth;
         if (mobileMenuDisplayed === true && width > 768) {
             toggleMenu(menu);
-            navbarMobileStyleUpdate(navbarLogo, navbarBar);
+            navbarMobileStyleUpdate(navbarLogo, navbarBar, borderHider);
         }
     }
 
@@ -43,6 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     btn.addEventListener('click', () => {
         toggleMenu(menu);
-        navbarMobileStyleUpdate(navbarLogo, navbarBar);
+        navbarMobileStyleUpdate(navbarLogo, navbarBar, borderHider);
     });
 });
