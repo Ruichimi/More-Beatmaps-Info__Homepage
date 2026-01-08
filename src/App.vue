971adec28a2c0eebe1626dc7ef25dad0b20c1b83
Ruichimi <thekeykeyloy@gmail.com>
2026-01-08 19:20:49 +0100

    Add star effect to the pages

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,17 @@ import Footer from '@/components/Footer.vue'
   <Navbar />
   <router-view />
   <Footer />
+
+  <div class="pointer-events-none absolute inset-0 overflow-hidden">
+      <span
+          v-for="i in 40"
+          :key="i"
+          class="absolute h-[5px] w-[5px] animate-pulse rounded-full bg-white/60"
+          :style="{
+          top: Math.random() * 100 + '%',
+          left: Math.random() * 100 + '%',
+          animationDelay: Math.random() * 3 + 's'
+        }"
+      />
+  </div>
 </template>
